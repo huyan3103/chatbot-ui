70c24ae8bc57e0c4df6d2ab3d2c81108e2dede65
huyan3103 <lehuyan04@gmail.com>
2021-12-15 13:49:40 +0700

    Update

--- src/components/Chatbot/ChatbotOpen.jsx
+++ src/components/Chatbot/ChatbotOpen.jsx
@@ -1,99 +1,96 @@
-import { useState, useEffect, useRef } from "react";
-import useGetAnswer from "../../hooks/use-getAnswer";
+import { useState, useEffect, useRef } from 'react'
+import useGetAnswer from '../../hooks/use-getAnswer'
 
-import ChatbotMessage from "./ChatbotMessage";
-import WaveMessage from "./WaveMessage";
+import ChatbotMessage from './ChatbotMessage'
+import WaveMessage from './WaveMessage'
 
-import "./ChatbotOpen.css";
+import './ChatbotOpen.css'
 
 const ChatbotOpen = ({ setIsOpen, zoom, handleToggleZoom, handleZoomOut }) => {
   const [conversation, setConversation] = useState(() => {
     // get conversation from sessionStorage
-    const saved = sessionStorage.getItem("conversation");
-    const initialValue = JSON.parse(saved);
-    return initialValue || [];
-  });
+    const saved = sessionStorage.getItem('conversation')
+    const initialValue = JSON.parse(saved)
+    return initialValue || []
+  })
 
-  const [enteredTextInput, setEnteredTextInput] = useState("");
+  const [enteredTextInput, setEnteredTextInput] = useState('')
 
   const addMessageToConversation = (message) => {
     setConversation((prevState) => [
       ...prevState,
       {
-        sender: "bot",
+        sender: 'bot',
         content: message,
       },
-    ]);
-  };
+    ])
+  }
 
-  const { isLoading, error, sendRequest } = useGetAnswer(
-    addMessageToConversation
-  );
+  const { isLoading, error, sendRequest } = useGetAnswer(addMessageToConversation)
 
-  const chatboxInnerRef = useRef();
+  const chatboxInnerRef = useRef()
 
   // auto scroll bottom message
   useEffect(() => {
     if (chatboxInnerRef.current) {
-      chatboxInnerRef.current.scrollTop = chatboxInnerRef.current.scrollHeight;
+      chatboxInnerRef.current.scrollTop = chatboxInnerRef.current.scrollHeight
     }
 
-    sessionStorage.setItem("conversation", JSON.stringify(conversation));
-  }, [conversation]);
+    sessionStorage.setItem('conversation', JSON.stringify(conversation))
+  }, [conversation])
 
   useEffect(() => {
-    sendRequest("hello");
-  }, []);
+    sendRequest('hello')
+  }, [])
 
   const handleCloseChatbox = () => {
-    handleZoomOut();
-    setIsOpen(false);
-  };
+    handleZoomOut()
+    setIsOpen(false)
+  }
 
   const handleChangeInput = (event) => {
-    setEnteredTextInput(event.target.value);
-  };
+    setEnteredTextInput(event.target.value)
+  }
 
   const handleSubmitForm = (event) => {
-    event.preventDefault();
+    event.preventDefault()
     if (enteredTextInput) {
       setConversation((prevState) => {
         return [
           ...prevState,
           {
-            sender: "user",
+            sender: 'user',
             content: enteredTextInput,
           },
-        ];
-      });
-      sendRequest(enteredTextInput);
+        ]
+      })
+      sendRequest(enteredTextInput)
     }
-    setEnteredTextInput("");
-  };
+    setEnteredTextInput('')
+  }
 
-  const iconZoomHeader = zoom ? (
-    <box-icon name="minus" color="white"></box-icon>
-  ) : (
-    <box-icon name="square" color="white"></box-icon>
-  );
+  const iconZoomHeader = zoom ? <box-icon name="minus" color="white"></box-icon> : <box-icon name="square" color="white"></box-icon>
 
   return (
     <div className="chatbox-wapper">
       <div className="chatbox-header">
         <p>Ultimate Chatbot For Education</p>
-        <div className="chatbox-btn" onClick={props.handleToggleZoom}>
-          {props.zoom ? <box-icon name="minus" color="white"></box-icon> : <box-icon name="square" color="white"></box-icon>}
+        <div className="chatbox-btn chatbox-btn__zoom" onClick={handleToggleZoom}>
+          {iconZoomHeader}
         </div>
         <div className="chatbox-btn" onClick={handleCloseChatbox}>
           <box-icon name="x" color="white"></box-icon>
         </div>
       </div>
       <div className="chatbox-inner" ref={chatboxInnerRef}>
+        {/* render message */}
         {conversation.map((message, index) => (
           <ChatbotMessage sender={message.sender} key={index}>
             {message.content}
           </ChatbotMessage>
         ))}
+
+        {/* render animation when loading */}
         {isLoading && (
           <ChatbotMessage sender="bot">
             <WaveMessage />
@@ -102,27 +99,14 @@ const ChatbotOpen = ({ setIsOpen, zoom, handleToggleZoom, handleZoomOut }) => {
       </div>
       <div className="chatbox-footer">
         <form onSubmit={handleSubmitForm}>
-          <input
-            type="text"
-            placeholder="Send something ..."
-            onChange={handleChangeInput}
-            value={enteredTextInput}
-          />
-          <button
-            className={`chatbox-btn ${enteredTextInput && "active"}`}
-            type="submit"
-            onClick={handleSubmitForm}
-          >
-            <box-icon
-              type="solid"
-              name="send"
-              color={`${enteredTextInput && "#d82c2c"}`}
-            ></box-icon>
+          <input type="text" placeholder="Send something ..." onChange={handleChangeInput} value={enteredTextInput} />
+          <button className={`chatbox-btn ${enteredTextInput && 'active'}`} type="submit" onClick={handleSubmitForm}>
+            <box-icon type="solid" name="send" color={`${enteredTextInput && '#d82c2c'}`}></box-icon>
           </button>
         </form>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ChatbotOpen;
+export default ChatbotOpen
