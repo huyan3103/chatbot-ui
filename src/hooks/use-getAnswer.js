6e46fca2c6250fc9148ae372344608a3c85779b0
huyan3103 <61448856+huyan3103@users.noreply.github.com>
2021-12-19 16:21:03 +0700

    Update use-getAnswer.js

--- src/hooks/use-getAnswer.js
+++ src/hooks/use-getAnswer.js
@@ -9,7 +9,7 @@ const useGetAnswer = (applyData) => {
     setError(null);
 
     try {
-      const response = await fetch("https://ultimate-chatbot.tk/", {
+      const response = await fetch("https://ultimate-chatbot.tk:8080/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question: input }),
